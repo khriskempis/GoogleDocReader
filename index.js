783fd2e2b42ae6a12daf1238747e07681499f11f
Khris Kempis <kkempis@centerfield.com>
2020-02-14 17:45:47 -0800

    fix format content

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ function getNewToken(oAuth2Client, callback) {
     });
   });
 }
-
 /**
  * Prints the title of a sample doc:
  * https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
@@ -81,8 +80,163 @@ function printDocmentContents(auth) {
   (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     var doc = res.data;
-    var content = JSON.stringify(doc.body, null, 4);
+    var data = doc.body.content;
+    var paragraph = FormatDocument(data)
 
-    console.log(`The title of the document is: ${res.data.title}`);
+    // console.log(`The title of the document is: ${res.data.title}`);
+    console.log(paragraph)
   });
+}
+
+const headerArray = [
+  "HEADING_1",
+  "HEADING_2",
+  "HEADING_3",
+  "HEADING_4",
+  "HEADING_5"
+]
+
+const BNdashboardFields = [
+  'city',
+  'state',
+  'intro',
+  'cheapestInternet',
+  'typesOfInternet',
+  'internetSpeed',
+  'fastestInternet',
+  'bundles',
+  'compareProviders',
+  'movingToCity',
+  'installationFees',
+  'howMuchData',
+  'mostConnected',
+  'internetStats',
+  'faqCheapestInternet',
+  'faqFastestInternet',
+  'faqNumberOfInternetProviders',
+  'faqNumberofResProviders',
+  'faqBundle',
+  'faqNumberOfBusProviders',
+  'Enabled',
+]
+
+
+function addHeaderElements(content){
+  let textStyle = content.paragraphStyle
+  let headerType = textStyle.namedStyleType;
+  let headerNumber = headerArray.indexOf(headerType) + 1
+  let headerHTMLopen = `<h${headerNumber}>`
+  let headerHTMLclose = `</h${headerNumber}>`
+  if(headerArray.includes(headerType)){
+    let headerTag = '';
+    let headerElements = content.elements
+    for(let i=0; i<headerElements.length; i++){
+      headerTag += headerElements[i].textRun.content;
+    }
+    return `${headerHTMLopen}${headerTag}${headerHTMLclose}`
+  } else {
+    return '' 
+  }
+}
+
+function extractCityAndState(section){
+  let cityStateString = section.elements[0].textRun.content; 
+  let index = cityStateString.indexOf("for");
+  cityStateString = cityStateString.slice(index + 4);
+  cityStateTrimmed = trimContent(cityStateString);
+  return cityStateTrimmed.split(", ");
+}
+
+function trimContent(content){
+  var stringArray = content.split(" ")
+    var trimmedContent = stringArray.map(word => {
+      if(word.includes("\n")){
+        return word.slice(0, word.length - 1);
+      } else {
+        return word
+      }
+    }).join(' ')
+  return trimmedContent
+}
+
+function formatContent(data){
+  let textStyle = data.paragraphStyle
+  if(textStyle.namedStyleType == "NORMAL_TEXT"){
+    let formatedString = '';
+    let elements = data.elements
+    elements.forEach(item => {
+      let content = item.textRun.content;
+      let isBold = item.textRun.textStyle.bold;
+      if(content.includes("\n")){
+        var trimmedContent = trimContent(content)
+        content = trimmedContent;
+      } 
+      if(isBold && content.length > 0){
+        formatedString += `<strong>${content}</strong>`
+      } else {
+        formatedString += content;
+      }
+    })
+    return formatedString
+  } else {
+    return '' 
+  }
+}
+
+function FormatDocument(content){
+  let BNobject = {};
+  let text = '';
+  let ulCounter = 0;
+  let index = 0
+  content.forEach(item => {
+    let section = item.paragraph;
+    if(section){
+      let textStyle = section.paragraphStyle.namedStyleType
+      //format headings
+      if(headerArray.includes(textStyle)){
+        // var formattedH1 = addHeaderElements(section);
+        // text += formattedH1;
+        // check if it's the main header with City and State, 
+        if(index < 1){
+          var cityState = extractCityAndState(section);
+          BNobject[BNdashboardFields[index]] = cityState[0];
+          BNobject[BNdashboardFields[index+1]] = cityState[1];
+          index+=1
+        } else {
+          BNobject[BNdashboardFields[index]] 
+        }
+        index+= 1
+      //format paragraph
+      } 
+      if (textStyle == "NORMAL_TEXT"){
+        let list = section.bullet
+        // check if it's a list
+          if(list){
+            // create ul 
+            if(ulCounter == 0){
+              text += "<ul>"
+            }
+            var formatedList = formatContent(section)
+            text += `<li> ${formatedList} </li>`
+
+            ulCounter += 1; 
+            if(ulCounter == 4){
+              text += "</ul>"
+              ulCounter = 0;
+              BNobject[BNdashboardFields[index]] = text;
+              index+=1
+              text = '';
+            }
+          } else {
+            var formattedParagraph = formatContent(section);
+            if(formattedParagraph.length > 0){
+              text += `<p>${formattedParagraph}</p>`;
+              BNobject[BNdashboardFields[index]] = text;
+              text = '';
+            }
+          }
+      }
+    }
+  })
+    return BNobject;
 }
