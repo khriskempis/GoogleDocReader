1875addb66891af042bb607d6b5209db9548b680
Khris Kempis <khriskempis@gmail.com>
2020-02-17 13:45:31 -0800

    add comments

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // const {CLIENT_ID, CLIENT_SECRET } = require("./config");
 
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/documents.readonly'];
+const SCOPES = ["https://www.googleapis.com/auth/documents.readonly"];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json';
+const TOKEN_PATH = "token.json";
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
+fs.readFile("credentials.json", (err, content) => {
+  if (err) return console.log("Error loading client secret file:", err);
   // Authorize a client with credentials, then call the Google Docs API.
   authorize(JSON.parse(content), printDocmentContents);
 });
@@ -25,9 +25,12 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
@@ -45,23 +48,23 @@ function authorize(credentials, callback) {
  */
 function getNewToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
+    access_type: "offline",
+    scope: SCOPES
   });
-  console.log('Authorize this app by visiting this url:', authUrl);
+  console.log("Authorize this app by visiting this url:", authUrl);
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
+    output: process.stdout
   });
-  rl.question('Enter the code from that page here: ', (code) => {
+  rl.question("Enter the code from that page here: ", code => {
     rl.close();
     oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
+      if (err) return console.error("Error retrieving access token", err);
       oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
         if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
+        console.log("Token stored to", TOKEN_PATH);
       });
       callback(oAuth2Client);
     });
@@ -74,15 +77,17 @@ function getNewToken(oAuth2Client, callback) {
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth 2.0 client.
  */
 function printDocmentContents(auth) {
-  const docs = google.docs({version: 'v1', auth});
-  docs.documents.get({
-    documentId: '1N1ox86Qzcu5YuJBlfMuPi3zJFEwtPU1Xp2tT1umBfc0',
-  }, 
-  (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    var doc = res.data;
-    var content = JSON.stringify(doc.body, null, 4);
+  const docs = google.docs({ version: "v1", auth });
+  docs.documents.get(
+    {
+      documentId: "1N1ox86Qzcu5YuJBlfMuPi3zJFEwtPU1Xp2tT1umBfc0"
+    },
+    (err, res) => {
+      if (err) return console.log("The API returned an error: " + err);
+      var doc = res.data;
+      var content = JSON.stringify(doc.body, null, 4);
 
-    console.log(`The title of the document is: ${res.data.title}`);
-  });
+      console.log(`The title of the document is: ${res.data.title}`);
+    }
+  );
 }
